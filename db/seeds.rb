2dd1d2ebfe66f798f5b637347f4e34986068a87e
Cyril <martinot.cyril@gmail.com>
2021-05-26 17:01:09 +0200

    yo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating flats...'
+
+london = { name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+}
+
+paris = { name: 'Beautiful flat on MontMartre',
+  address: 'MontMartre, Paris',
+  description: 'A lovely winter.Two double bedrooms, 12 living rooms',
+  price_per_night: 300,
+  number_of_guests: 7
+}
+
+berlin = { name: 'Techno unz unz',
+  address: 'Berghain, Berlin',
+  description: 'Nice, respectful neighbourhood',
+  price_per_night: 175,
+  number_of_guests: 18
+}
+
+phnom_penh = { name: 'View over the Mekong',
+  address: '51 Pasteur Street, Phnom Penh',
+  description: 'Lowkey sweet',
+  price_per_night: 37,
+  number_of_guests: 4
+}
+
+[london, berlin, paris, phnom_penh].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts 'Finished!'
+
